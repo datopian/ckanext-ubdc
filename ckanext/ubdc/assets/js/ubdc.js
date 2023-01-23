23de15db8d3d744255c60cc17d5c26aa23968b14
sagargg <info@sagarg.com.np>
2023-01-23 14:15:22 +0545

    [improve][s]: added collapse on dataset fields metadata

--- ckanext/ubdc/assets/js/ubdc.js
+++ ckanext/ubdc/assets/js/ubdc.js
@@ -1,3 +1,16 @@
 $('.site-search button').click(function(event) {
     $('#field-sitewide-search').addClass('');
 });
+
+// toggle view full or
+$('.view_more_fields').click(function(event) {
+    event.preventDefault();
+    $('.repeated_dataset_fields').toggleClass("more");
+    // change text
+    if ($('.repeated_dataset_fields').hasClass("more")) {
+        $(this).text("View less");
+    }
+    else {
+        $(this).text("View more");
+    }
+});
